b3b47ce938e9313a5ed75c435d6a95e6f0c445ee
An chang wan <ssdii66@gmail.com>
2023-02-24 00:28:43 +0900

    tailwind pratice

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           <span className="font-semibold">$80</span>
         </div>
         <div className="my-3 flex justify-between border-t-2 border-dashed">
-          <span class="text-3xl font-semibold">Total</span>s
+          <span className="text-3xl font-semibold">Total</span>s
           <span className="font-semibold">$89</span>
         </div>
         <div className="mx-auto mt-5 w-2/4 rounded-xl bg-blue-500 p-3 text-center text-white">
@@ -42,7 +42,52 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="rounded-2xl bg-white p-10 shadow-lg"></div>
+      <div className="rounded-2xl bg-white p-10 shadow-lg">
+        <div className="py-1">
+          <div className="mb-2 flex justify-between">
+            <span>⬅️</span>
+            <div className="flex items-center justify-center">
+              <span>⭐️</span>
+              <span className="mr-2">4.9</span>
+              <span>💖</span>
+            </div>
+          </div>
+          <div className="flex items-center justify-center">
+            <img
+              src="https://i.ibb.co/5963kz8/chair.jpg"
+              className="mb-2 bg-gray-500 bg-auto"
+            />
+          </div>
+          <div>
+            <div className="flex flex-col">
+              <span className="text-2xl font-medium">Swoon Lounge</span>
+              <span className="text-xl text-gray-400">Chair</span>
+            </div>
+            <div className="flex justify-between py-3 ">
+              <div className="space-x-2">
+                <button className="h-5 w-5 rounded-full bg-orange-400 ring-orange-400 ring-offset-2 focus:ring-2"></button>
+                <button className="h-5 w-5 rounded-full bg-teal-400 ring-teal-400 ring-offset-2 focus:ring-2"></button>
+                <button className="h-5 w-5 rounded-full bg-red-400 ring-red-400 ring-offset-2 focus:ring-2"></button>
+              </div>
+              <div className="mr-8 flex space-x-2">
+                <button className="flex aspect-square items-center justify-center rounded-md bg-blue-400">
+                  -
+                </button>
+                <span>1</span>
+                <button className="flex aspect-square items-center justify-center rounded-md bg-blue-400">
+                  +
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-2xl font-medium">$450</span>
+            <button className="rounded-xl bg-blue-500 px-5 py-2 text-xl font-medium text-white">
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="rounded-2xl bg-white p-10 shadow-lg"></div>
     </div>
   );
