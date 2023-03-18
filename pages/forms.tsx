057b84205995ab5bb7bc63ffd9cb20606042db77
An chang wan <ssdii66@gmail.com>
2023-03-19 00:03:09 +0900

    유효ㅕ성 검사

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,29 +1,52 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, FieldError } from "react-hook-form";
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email?: string;
+}
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm();
-  const onValid = () => {
-    console.log("유효");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({ mode: "onBlur" });
+  const onValid = (data: LoginForm) => {
+    console.log(data);
   };
+  const onInvalid = (errors: FieldError) => {
+    console.log(errors);
+  };
+
   return (
-    <form onSubmit={handleSubmit(onValid)}>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
-        {...register("username", { required: true })}
+        {...register("username", {
+          required: "this is error",
+          minLength: { message: "5글장 이상 적어주세요", value: 5 },
+        })}
         type="text"
         placeholder="Username"
       />
       <input
-        {...(register("email"), { required: true })}
+        {...register("email", {
+          required: "this is error",
+          validate: {
+            notGmail: (value) =>
+              !value.includes("@gmail.com") || "Gmail is not allowed",
+          },
+        })}
         type="email"
         placeholder="email"
       />
+      {errors.email?.message}
       <input
-        {...(register("password"), { required: true })}
+        {...register("password", { required: "this is error" })}
         type="password"
         placeholder="password"
       />
-      <input {...register("submit")} type="submit" />
+      <input type="submit" value="계정 생성" />
     </form>
   );
 }
