2a7d128f30afb8835eb0bdb84b3b74f6ed25ec0d
Anchangwan <ssdii44@naver.com>
2023-03-22 17:05:34 +0900

    api/enter handler 작업

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,7 +3,57 @@ import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandle";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  let user;
+  user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "익명",
+      ...payload,
+    },
+    update: {},
+  });
+
+  console.log(user);
+  // if (email) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       email,
+  //     },
+  //   });
+  //   if (user) console.log("found it.");
+  //   if (!user) {
+  //     console.log("did not find");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "익명",
+  //         email,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
+  // if (phone) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       phone: +phone,
+  //     },
+  //   });
+  //   if (user) console.log("found it.");
+  //   if (!user) {
+  //     console.log("did not find");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "익명",
+  //         phone: +phone,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
   res.status(200).end();
 }
 
